96335a8e6f564b0654b9e430eeb4e017c4522e70
abangdsm <id.reversal@gmail.com>
2025-05-29 23:26:20 +0700

    Menambahkan halaman admin secara parsial

--- resources/views/livewire/admin.blade.php
+++ resources/views/livewire/admin.blade.php
@@ -1,3 +1,19 @@
-<div>
-    {{-- In work, do what you enjoy. --}}
+<div class="container-scroller">
+    @include('livewire.partials.navbar')
+
+    <!-- partial -->
+    <div class="container-fluid page-body-wrapper">
+        @include('livewire.partials.sidebar')
+
+        <!-- partial -->
+        <div class="main-panel">
+            <div class="content-wrapper">
+                <h4>Halo Programmers!</h4>
+            </div>
+            <!-- content-wrapper ends -->
+        </div>
+        <!-- main-panel ends -->
+    </div>
+
+    <!-- page-body-wrapper ends -->
 </div>
---
+++ resources/views/livewire/partials/sidebar.blade.php
@@ -0,0 +1,66 @@
+<!-- partial:partials/_sidebar.html -->
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+    <ul class="nav">
+        <li class="nav-item nav-category">Home</li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="/fp-admin">
+                <i class="mdi mdi-grid-large menu-icon"></i>
+                <span class="menu-title">Dashboard</span>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="#">
+                <i class="menu-icon mdi mdi-chart-line"></i>
+                <span class="menu-title">Sales</span>
+            </a>
+        </li>
+
+        <li class="nav-item nav-category">Funnel Page</li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="#">
+                <i class="menu-icon mdi mdi-file-document"></i>
+                <span class="menu-title">Create Page</span>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false"
+                aria-controls="ui-basic">
+                <i class="menu-icon mdi mdi-floor-plan"></i>
+                <span class="menu-title">Templates</span>
+                <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="ui-basic">
+                <ul class="nav flex-column sub-menu">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Template 1</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Template 2</a>
+                    </li>
+                </ul>
+            </div>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
+                <i class="menu-icon mdi mdi-account-circle-outline"></i>
+                <span class="menu-title">Members</span>
+                <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="auth">
+                <ul class="nav flex-column sub-menu">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"> Member List </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"> Database </a>
+                    </li>
+                </ul>
+            </div>
+        </li>
+    </ul>
+</nav>
