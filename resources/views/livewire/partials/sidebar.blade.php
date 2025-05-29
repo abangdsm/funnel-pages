974790f915dcd369b9e35dee5fc569dc6defad08
abangdsm <id.reversal@gmail.com>
2025-05-29 23:31:34 +0700

    Menyesuaikan sidebar

--- resources/views/livewire/partials/sidebar.blade.php
+++ resources/views/livewire/partials/sidebar.blade.php
@@ -1,8 +1,7 @@
 <!-- partial:partials/_sidebar.html -->
 <nav class="sidebar sidebar-offcanvas" id="sidebar">
     <ul class="nav">
-        <li class="nav-item nav-category">Home</li>
-
+        
         <li class="nav-item">
             <a class="nav-link" href="/fp-admin">
                 <i class="mdi mdi-grid-large menu-icon"></i>
